979a9cc88c8bc494a62f08c97661a89a78101d90
indiepopart <jimena@gmail.com>
2023-06-20 19:38:38 -0300

    allow cors

--- src/main/java/com/example/demo/GraphQlConfig.java
+++ src/main/java/com/example/demo/GraphQlConfig.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration(proxyBeanMethods = false)
 class GraphQlConfig {
@@ -17,5 +19,15 @@ class GraphQlConfig {
                 builder.inspectSchemaMappings(report -> {
                     logger.debug(report.toString());
                 });
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/graphql").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
