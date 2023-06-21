f12fa97a880fc8c1ca0f508c9e78e88bd004d50a
indiepopart <jimena@gmail.com>
2023-06-21 15:33:36 -0300

    add company count call

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Company;
 import com.example.demo.domain.Person;
 import com.example.demo.repository.CompanyRepository;
 import com.example.demo.repository.PersonRepository;
@@ -8,7 +9,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
-import com.example.demo.domain.Company;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -41,13 +41,20 @@ public class CompanyController {
     public Mono<Person> personById(@Argument Long id) {
         return personRepository.findById(id);
     }
+
     @SchemaMapping
-    public Flux<Company> controls(Person person){
+    public Flux<Company> controls(Person person) {
         return companyRepository.findByControlledBy(person.getName());
     }
 
     @QueryMapping
-    public Flux<Company> company(@Argument Long page){
-        return companyRepository.findAll().skip(page*10).take(10);
+    public Flux<Company> company(@Argument Long page) {
+        return companyRepository.findAll().skip(page * 10).take(10);
     }
+
+    @QueryMapping
+    public Mono<Long> companyCount() {
+        return companyRepository.count();
+    }
+
 }
