c5511d5edb70381286ae087c31d3a5dc70f2bb9b
indiepopart <jimena@gmail.com>
2023-06-24 21:12:32 -0300

    clean up

--- src/main/java/com/example/demo/repository/CompanyRepository.java
+++ src/main/java/com/example/demo/repository/CompanyRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.neo4j.repository.query.Query;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import org.springframework.data.domain.Pageable;
+
 public interface CompanyRepository extends ReactiveNeo4jRepository<Company, Long> {
 
     Mono<Company> findByName(String name);
 
     @Query("MATCH (c:Company) WHERE (:Person {name: $name})-[:HAS_CONTROL]->(c) RETURN c")
     Flux<Company> findByControlledBy(String name);
-
 
 }
